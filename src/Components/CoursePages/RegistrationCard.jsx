7fed69ff886e7b1c55b22114188035f6e9ef0c08
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-07-22 18:06:14 +0500

    working with the web dev course sec

--- src/Components/CoursePages/RegistrationCard.jsx
+++ src/Components/CoursePages/RegistrationCard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const RegistrationCard = () => {
-  return <div className=""></div>;
+  return (
+    <div className="registration-card">
+      <div className="reg-content">
+        <h1>Getting Started with Front-End Web Development</h1>
+        <p>
+          Slots Available <span>30/30</span>
+        </p>
+        <p>
+          <span></span> 4.5 Months
+        </p>
+        <p>
+          <span></span>
+        </p>
+        <p>
+          <span></span>
+        </p>
+        <p>
+          <span></span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default RegistrationCard;
